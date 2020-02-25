86e0dc3156a746b2510f063b3fc55d835b6879cb
Pierre Henry <pierre.henry@salsaaddict.com>
2020-02-25 12:33:51 +0000

    Initial Setup

--- scripts/emusicality.js
+++ scripts/emusicality.js
@@ -1,2 +1,36 @@
 "use strict";
-var app = angular.module("emusicality", ["ngRoute", "ngSanitize"]);
+var Application;
+(function (Application) {
+    var debugEnabled = true;
+    function config() {
+        var fn = function ($logProvider) {
+            $logProvider.debugEnabled(debugEnabled);
+        };
+        fn.$inject = ["$logProvider"];
+        return fn;
+    }
+    Application.config = config;
+    ;
+    function run() {
+        var fn = function ($log) {
+            $log.debug("emu:run");
+        };
+        fn.$inject = ["$log"];
+        return fn;
+    }
+    Application.run = run;
+    ;
+    var Player;
+    (function (Player) {
+        var Controller = /** @class */ (function () {
+            function Controller($scope) {
+            }
+            Controller.$inject = ["$scope"];
+            return Controller;
+        }());
+        Player.Controller = Controller;
+    })(Player = Application.Player || (Application.Player = {}));
+})(Application || (Application = {}));
+angular.module("emusicality", ["ngRoute", "ngSanitize"])
+    .config(Application.config())
+    .run(Application.run());
--- scripts/emusicality.ts
+++ scripts/emusicality.ts
@@ -1,1 +1,27 @@
-let app: ng.IModule = angular.module("emusicality", ["ngRoute", "ngSanitize"]);
+namespace Application {
+    const debugEnabled: boolean = true;
+    export function config(): ng.Injectable<Function> {
+        let fn: ng.Injectable<Function> = function ($logProvider: ng.ILogProvider): void {
+            $logProvider.debugEnabled(debugEnabled);
+        };
+        fn.$inject = ["$logProvider"];
+        return fn;
+    };
+    export function run(): ng.Injectable<Function> {
+        let fn: ng.Injectable<Function> = function ($log: ng.ILogService): void {
+            $log.debug("emu:run");
+        };
+        fn.$inject = ["$log"];
+        return fn;
+    };
+    export namespace Player {
+        export class Controller implements ng.IController {
+            static $inject: string[] = ["$scope"];
+            constructor($scope: ng.IScope) { }
+        }
+    }
+}
+
+angular.module("emusicality", ["ngRoute", "ngSanitize"])
+    .config(Application.config())
+    .run(Application.run());
